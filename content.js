3243eb552df488df5e92043b044fbdcba03a6221
Konstantin Antonov <konstantin.m.antonov@gmail.com>
2023-06-20 08:13:22 +0300

    update UI

--- content.js
+++ content.js
@@ -1,4 +1,9 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+// Handle the message here, e.g., switch AWS region
+sendResponse({ success: true });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const newRegion = request.region;
     const url = window.location.href;
     const updatedUrl = url.replace(/(region=)(\w+-\w+-\d+)/, `$1${newRegion}`);
--- popup.js
+++ popup.js
@@ -4,4 +4,15 @@ document.getElementById('setRegion').addEventListener('click', () => {
       chrome.tabs.sendMessage(tabs[0].id, { region: selectedRegion });
     });
   });
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'switchRegion', region: 'us-east-1' }, (response) => {
+        // Handle the response from the content script, e.g., show a success message
+        if (response.success) {
+            const successMessage = document.getElementById('success-message');
+            successMessage.textContent = 'Region switched successfully!';
+            successMessage.style.display = 'block';
+        }
+    });
+});
   
